7ab14c3f1c8ec68ad4b51123e179f82711c0e713
William <william.teletabs@hotmail.com>
2022-12-15 10:03:59 -0300

    Criando classe Slide

---
+++ src/slide.ts
@@ -0,0 +1,16 @@
+export default class Slide {
+  container;
+  elements;
+  controls;
+  time;
+    constructor(container: Element, 
+    elements: Element[],
+    controls: Element, 
+    time: number = 5000
+  ) {
+    this.container = container;
+    this.elements = elements;
+    this.controls = controls;
+    this.time = time;
+  }
+}
