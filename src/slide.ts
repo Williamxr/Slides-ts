3290c679e755aa57c7ca4c73ebae6e0b7f52386b
William <william.teletabs@hotmail.com>
2022-12-15 16:03:50 -0300

    Criando timeout para o slide passar automaticamente e ser possível pausar o mesmo

--- src/slide.ts
+++ src/slide.ts
@@ -1,3 +1,5 @@
+import Timeout from "./Timeout.js";
+
 export default class Slide {
   container;
   slides;
@@ -5,6 +7,7 @@ export default class Slide {
   time;
   index;
   slide;
+  timeout: Timeout | null;
     constructor(container: Element, 
     slides: Element[],
     controls: Element, 
@@ -15,6 +18,7 @@ export default class Slide {
     this.controls = controls;
     this.time = time;
 
+    this.timeout = null;
     this.index = 0;
     this.slide = this.slides[this.index];
 
@@ -30,6 +34,12 @@ export default class Slide {
     this.slide = this.slides[this.index];
     this.slides.forEach((el) => this.hide(el));
     this.slide.classList.add('active');
+    this.auto(this.time)
+  }
+
+  auto(time: number) {
+    this.timeout?.clear();
+    this.timeout = new Timeout(() => this.next(), time);
   }
 
   prev() {
