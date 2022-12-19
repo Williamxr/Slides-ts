154071415bf1e4a21cbb22dd13eb84bccac15768
William <william.teletabs@hotmail.com>
2022-12-19 18:06:09 -0300

    Adicionando funções pause e continue para calcular o tempo de pausa do slide

--- src/slide.ts
+++ src/slide.ts
@@ -1,89 +1,83 @@
 import Timeout from "./Timeout.js";
 
 export default class Slide {
-  container;
-  slides;
-  controls;
-  time;
-  index;
-  slide;
+  container: Element;
+  slides: Element[];
+  controls: Element;
+  time: number;
+  index: number;
+  slide: Element;
   timeout: Timeout | null;
   pausedTimeout: Timeout | null;
   paused: boolean;
-    constructor(container: Element, 
+  constructor(
+    container: Element,
     slides: Element[],
-    controls: Element, 
+    controls: Element,
     time: number = 5000
   ) {
     this.container = container;
     this.slides = slides;
     this.controls = controls;
     this.time = time;
+
     this.timeout = null;
     this.pausedTimeout = null;
     this.index = 0;
     this.slide = this.slides[this.index];
     this.paused = false;
+
     this.init();
   }
-
   hide(el: Element) {
-    el.classList.remove('active')
+    el.classList.remove("active");
   }
-
   show(index: number) {
     this.index = index;
     this.slide = this.slides[this.index];
     this.slides.forEach((el) => this.hide(el));
-    this.slide.classList.add('active');
-    this.auto(this.time)
+    this.slide.classList.add("active");
+    this.auto(this.time);
   }
-
   auto(time: number) {
     this.timeout?.clear();
     this.timeout = new Timeout(() => this.next(), time);
   }
-
   prev() {
-    if(this.paused) return;
+    if (this.paused) return;
     const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
     this.show(prev);
   }
-
   next() {
-    if(this.paused) return;
+    if (this.paused) return;
     const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
     this.show(next);
   }
-
-  pause(){
+  pause() {
     this.pausedTimeout = new Timeout(() => {
+      this.timeout?.pause();
       this.paused = true;
     }, 300);
-    this.paused = true;
   }
-
-  continue(){
-    this.pausedTimeout?.clear;
-    if(this.paused){
+  continue() {
+    this.pausedTimeout?.clear();
+    if (this.paused) {
       this.paused = false;
-      this.auto(this.time);
+      this.timeout?.continue();
     }
   }
-
   private addControls() {
-    const prevButton = document.createElement('button');
-    const nextButton = document.createElement('button');
-    prevButton.innerText = 'Prev';
-    nextButton.innerText = 'Next';
+    const prevButton = document.createElement("button");
+    const nextButton = document.createElement("button");
+    prevButton.innerText = "Slide Anterior";
+    nextButton.innerText = "Próximo Slide";
     this.controls.appendChild(prevButton);
     this.controls.appendChild(nextButton);
 
-    this.controls.addEventListener('pointerdown', () => this.pause());
-    this.controls.addEventListener('pointerup', () => this.continue());
-
-    prevButton.addEventListener('pointerup', () => this.prev());
-    nextButton.addEventListener('pointerup', () => this.next());
+    this.controls.addEventListener("pointerdown", () => this.pause());
+    this.controls.addEventListener("pointerup", () => this.continue());
+    prevButton.addEventListener("pointerup", () => this.prev());
+    nextButton.addEventListener("pointerup", () => this.next());
   }
   private init() {
     this.addControls();
