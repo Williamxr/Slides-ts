907a436396d43859cf1d47f8066dd9611e092a89
William <william.teletabs@hotmail.com>
2023-01-17 22:35:20 -0300

    Atualizando js

--- dist/slide.js
+++ dist/slide.js
@@ -110,6 +110,7 @@ export default class Slide {
         this.controls.appendChild(nextButton);
         this.controls.addEventListener("pointerdown", () => this.pause());
         document.addEventListener("pointerup", () => this.continue());
+        document.addEventListener("touched", () => this.continue());
         prevButton.addEventListener("pointerup", () => this.prev());
         nextButton.addEventListener("pointerup", () => this.next());
     }
