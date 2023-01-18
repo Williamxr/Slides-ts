ea79c4978e914195fc3cdd63720b840b9485d168
William <william.teletabs@hotmail.com>
2023-01-17 22:35:13 -0300

    Corrigindo nome do evento

--- src/slide.ts
+++ src/slide.ts
@@ -115,7 +115,7 @@ export default class Slide {
 
     this.controls.addEventListener("pointerdown", () => this.pause());
     document.addEventListener("pointerup", () => this.continue());
-    document.addEventListener("touched", () => this.continue());
+    document.addEventListener("touchend", () => this.continue());
     prevButton.addEventListener("pointerup", () => this.prev());
     nextButton.addEventListener("pointerup", () => this.next());
   }
